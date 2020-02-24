f8e331e1c77e6cc4e4d149ecf3cdd18a8ab01f34
Vivica Ma <vivicacongma@gmail.com>
2020-02-24 03:14:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -18,8 +18,17 @@
 
 def reverse_each_word(sentence)
   
-    (sentence.split(' ')).each do |word|
-    puts word.
-    
+    sentence.split(" ").each do |words|
+    sentence << words.reverse 
   end 
+    sentence.join(" ")
 end 
+
+
+#   original_array = string.split(" ")
+#   return_array = []
+#   original_array.each do|string|
+#     return_array << string.reverse
+#   end
+#   return_array.join(" ")
+# end
